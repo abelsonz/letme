fd1b2c552928773b9bd16e7da1f4c53c0c2e182f
abelsonz <113365713+abelsonz@users.noreply.github.com>
2023-12-05 11:56:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,92 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('search-input').focus();
 });
 
-document.getElementById('amazon-logo').addEventListener('click', function() {
-    selectLogo('amazon-logo');
-});
 
-document.getElementById('youtube-logo').addEventListener('click', function() {
-    selectLogo('youtube-logo');
-});
+function addEventListenersToLogos() {
+    const logoElements = document.querySelectorAll('.logo');
 
-document.getElementById('yelp-logo').addEventListener('click', function() {
-    selectLogo('yelp-logo');
-});
-
-document.getElementById('ebay-logo').addEventListener('click', function() {
-    selectLogo('ebay-logo');
-});
-
-document.getElementById('etsy-logo').addEventListener('click', function() {
-    selectLogo('etsy-logo');
-});
-
-document.getElementById('tripadvisor-logo').addEventListener('click', function() {
-    selectLogo('tripadvisor-logo');
-});
-
-document.getElementById('indeed-logo').addEventListener('click', function() {
-    selectLogo('indeed-logo');
-});
-
-document.getElementById('webmd-logo').addEventListener('click', function() {
-    selectLogo('webmd-logo');
-});
-
-document.getElementById('search-button').addEventListener('click', function() {
-    performSearch();
-});
-
-document.getElementById('reddit-logo').addEventListener('click', function() {
-    selectLogo('reddit-logo');
-});
-
-document.getElementById('googlemaps-logo').addEventListener('click', function() {
-    selectLogo('googlemaps-logo');
-});
-
-document.getElementById('wikipedia-logo').addEventListener('click', function() {
-    selectLogo('wikipedia-logo');
-});
-
-document.getElementById('pinterest-logo').addEventListener('click', function() {
-    selectLogo('pinterest-logo');
-});
-
-document.getElementById('imdb-logo').addEventListener('click', function() {
-    selectLogo('imdb-logo');
-});
-
-document.getElementById('github-logo').addEventListener('click', function() {
-    selectLogo('github-logo');
-});
-
-document.getElementById('spotify-logo').addEventListener('click', function() {
-    selectLogo('spotify-logo');
-});
-
-document.getElementById('allrecipes-logo').addEventListener('click', function() {
-    selectLogo('allrecipes-logo');
-});
-
-document.getElementById('goodreads-logo').addEventListener('click', function() {
-    selectLogo('goodreads-logo');
-});
-
-document.getElementById('craigslist-logo').addEventListener('click', function() {
-    selectLogo('craigslist-logo');
-});
-
-document.getElementById('carsandbids-logo').addEventListener('click', function() {
-    selectLogo('carsandbids-logo');
-});
-
-document.getElementById('theverge-logo').addEventListener('click', function() {
-    selectLogo('theverge-logo');
-});
+    logoElements.forEach(logo => {
+        logo.addEventListener('click', function() {
+            selectLogo(logo.id);
+        });
+    });
+}
 
 function selectLogo(logoId) {
-    document.querySelectorAll('.logo').forEach(logo => logo.classList.remove('selected'));
+    document.querySelectorAll('.logo').forEach(logo => {
+        logo.classList.remove('selected');
+    });
     document.getElementById(logoId).classList.add('selected');
 }
 
@@ -97,71 +26,43 @@ function performSearch() {
     const selectedLogo = document.querySelector('.logo.selected');
 
     if (selectedLogo) {
-        let baseUrl;
-        switch (selectedLogo.id) {
-            case 'amazon-logo':
-                baseUrl = 'https://www.amazon.com/s?k=';
-                break;
-            case 'youtube-logo':
-                baseUrl = 'https://www.youtube.com/results?search_query=';
-                break;
-            case 'yelp-logo':
-                baseUrl = 'https://www.yelp.com/search?find_desc=';
-                break;
-            case 'ebay-logo':
-                baseUrl = 'https://www.ebay.com/sch/i.html?_nkw=';
-                break;
-            case 'etsy-logo':
-                baseUrl = 'https://www.etsy.com/search?q=';
-                break;
-            case 'tripadvisor-logo':
-                baseUrl = 'https://www.tripadvisor.com/search?q=';
-                break;
-            case 'indeed-logo':
-                baseUrl = 'https://www.indeed.com/jobs?q=';
-                break;
-            case 'webmd-logo':
-                baseUrl = 'https://www.webmd.com/search/search_results/default.aspx?query=';
-                break;
-            case 'reddit-logo':
-                baseUrl = 'https://www.reddit.com/search/?q=';
-                break;
-            case 'googlemaps-logo':
-                baseUrl = 'https://www.google.com/maps/search/';
-                break;
-            case 'wikipedia-logo':
-                baseUrl = 'https://en.wikipedia.org/wiki/Special:Search/';
-                break;
-            case 'pinterest-logo':
-                baseUrl = 'https://www.pinterest.com/search/pins/?q=';
-                break;
-            case 'imdb-logo':
-                baseUrl = 'https://www.imdb.com/find?q=';
-                break;
-            case 'github-logo':
-                baseUrl = 'https://github.com/search?q=';
-                break;
-            case 'spotify-logo':
-                baseUrl = 'https://open.spotify.com/search/';
-                break;
-            case 'allrecipes-logo':
-                baseUrl = 'https://www.allrecipes.com/search?q=';
-                break;
-            case 'goodreads-logo':
-                baseUrl = 'https://www.goodreads.com/search?q=';
-                break;
-            case 'craigslist-logo':
-                baseUrl = 'https://www.craigslist.org/search/sss?query=';
-                break;
-            case 'carsandbids-logo':
-                baseUrl = 'https://carsandbids.com/search/?q=';
-                break;
-            case 'theverge-logo':
-                baseUrl = 'https://www.theverge.com/search?q=';
-                 break;
+        const baseUrls = {
+            'amazon-logo': 'https://www.amazon.com/s?k=',
+            'youtube-logo': 'https://www.youtube.com/results?search_query=',
+            'yelp-logo': 'https://www.yelp.com/search?find_desc=',
+            'ebay-logo': 'https://www.ebay.com/sch/i.html?_nkw=',
+            'etsy-logo': 'https://www.etsy.com/search?q=',
+            'tripadvisor-logo': 'https://www.tripadvisor.com/search?q=',
+            'indeed-logo': 'https://www.indeed.com/jobs?q=',
+            'webmd-logo': 'https://www.webmd.com/search/search_results/default.aspx?query=',
+            'reddit-logo': 'https://www.reddit.com/search/?q=',
+            'googlemaps-logo': 'https://www.google.com/maps/search/',
+            'wikipedia-logo': 'https://en.wikipedia.org/wiki/Special:Search/',
+            'pinterest-logo': 'https://www.pinterest.com/search/pins/?q=',
+            'imdb-logo': 'https://www.imdb.com/find?q=',
+            'github-logo': 'https://www.github.com/saerch?q=',
+            'spotify-logo': 'https://open.spotify.com/search/',
+            'allrecipes-logo': 'https://www.allrecipes.com/search?q=',
+            'goodreads-logo': 'https://www.goodreads.com/search?q=',
+            'craigslist-logo': 'https://www.craigslist.org/search/sss?query=',
+            'carsandbids-logo': 'https://carsandbids.com/search/?q=',
+            'theverge-logo': 'https://www.theverge.com/search?q='
+        };
+
+        const baseUrl = baseUrls[selectedLogo.id];
+        if (baseUrl) {
+            window.location.href = baseUrl + searchQuery;
+        } else {
+            alert('Invalid search platform selected.');
         }
-        window.location.href = baseUrl + searchQuery;
     } else {
         alert('Please select a search platform.');
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('search-input').focus();
+    addEventListenersToLogos();
+});
+
+document.getElementById('search-button').addEventListener('click', performSearch);
